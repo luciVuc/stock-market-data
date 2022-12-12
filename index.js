546b0fd01f9993f433929d9c4d678a8bad8a1748
Lucian Vuc <lucian.vuc@gmail.com>
2022-12-12 02:37:40 -0800

    try deta

---
+++ index.js
@@ -0,0 +1,10 @@
+// // install express with `npm install express` 
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// // export 'app'
+// module.exports = app
+
+module.exports = require('./dist');
